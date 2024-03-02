3192035014125fd44e8141525d161aeec82b4324
Simran3611 <sipatel@siue.edu>
2024-03-02 11:14:50 -0600

    loginConnected

--- my-app/src/lib/components/navbar.svelte
+++ my-app/src/lib/components/navbar.svelte
@@ -23,7 +23,7 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="#footer">Contact</a>
+						<a class="nav-link" href="/login">Login</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#pricing">Pricing</a>
---
+++ my-app/src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    // import Login from "$lib/components/login"
+    import Login from '$lib/components/login.svelte';
+    
+</script>
+<Login />

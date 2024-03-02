c0d883874ebdbd9c4995bcff60d7a40685846e67
Maxwell Zinzilieta <mr.zinzilieta@gmail.com>
2024-03-02 15:57:32 -0600

    about page halfway done

---
+++ my-app/src/routes/about/+page.svelte
@@ -0,0 +1,102 @@
+<section id="title">
+	<div class="container-fluid">
+		<!-- Nav Bar -->
+		<nav class="navbar navbar-expand-lg navbar-dark">
+			<!-- Hamburger Menu Button -->
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="">Hack Together</a>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="/login">Login</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/signup">SignUp</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#cta">Download</a>
+					</li>
+					<li class="nav-item aboutLink">
+						<a class="nav-link" href="/about">About</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<!-- Title -->
+		<div class="titleBS">
+			<h1>What is Hack Together?</h1>
+		</div>
+	</div>
+</section>
+
+<div class="">
+
+</div>
+
+<style>
+	#title {
+		background-color: #000000;
+		color: #fff;
+	}
+
+	.aboutLink {
+		position: relative;
+		z-index: 77;
+	}
+
+	h1 {
+		font-family: 'Montserrat', sans-serif;
+		font-size: 3.5rem;
+		font-weight: 900;
+		line-height: 1.5;
+	}
+
+	.container-fluid {
+		padding: 3% 15% 7%;
+	}
+
+	/* Navigation Bar */
+	.navbar {
+		padding: 0 0 4.5rem;
+	}
+
+	.navbar-brand {
+		font-family: 'Ubuntu', sans-serif;
+		font-size: 2.5rem;
+		font-weight: bold;
+	}
+
+	.nav-item {
+		padding: 0 18px;
+	}
+
+	.nav-link {
+		font-size: 1.2rem;
+		font-family: 'Montserrat', sans-serif;
+		font-weight: 300;
+	}
+
+	/* Download Buttons */
+	.download-button {
+		margin: 5% 3% 5% 0;
+	}
+
+	.iphone-image {
+		width: 60%;
+		transform: rotate(25deg);
+		position: absolute;
+		right: -12%;
+	}
+</style>

759d124d5ff83f90e3fa9999894b184e97d9824c
Maxwell Zinzilieta <mr.zinzilieta@gmail.com>
2024-03-02 13:28:44 -0600

    removed skeleton ui

---
+++ my-app/.svelte-kit/generated/client/app.js
@@ -0,0 +1,31 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [4],
+		"/about": [5,[2]],
+		"/login": [6],
+		"/signup": [7],
+		"/swiper": [8,[3]]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';

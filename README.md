be9c6126623ab6fd504a5c2172b8e99e93f3d920
Chewbacca <thebopperm@gmail.com>
2024-03-02 05:51:46 -0600

    Initial Random Functions for MTD
    Added DuckeyScript Linguist detection

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Toegether
 Bringing people together two toes at a time. &lt;3
+
+![Richard_Stallman_Sticker](documents/stallman_sexy.png)
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Endpoint routes to file names i.e. Key : Value
 func setupStaticFiles(app *gin.Engine) {
@@ -16,10 +24,54 @@ func setupStaticFiles(app *gin.Engine) {
 	}
 }
 
+const charsetAlphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
 func main() {
 
 	app := gin.Default()
 	setupStaticFiles(app)
-	app.Run(":8080")
 
+	// Generate a random string
+	randomStr := randomString(10, charsetAlphaNum)
+	// Seed the randomness with the generated string
+	seedRandomnessWithString(randomStr)
+	randomDuration := generateRandomTimeInterval()
+	fmt.Println("Random Time Interval:", randomDuration)
+
+	// Default Route Port
+	app.Run(":8080")
 }
+
+// randomString generates a random string of length n using a specified character set.
+func randomString(n int, charset string) string {
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// seedRandomnessWithString takes a string, hashes it, and uses it to seed the random number generator.
+func seedRandomnessWithString(s string) {
+	hash := sha256.Sum256([]byte(s))
+	seed := int64(binary.BigEndian.Uint64(hash[:8]))
+	rand.Seed(seed)
+}
+
+func generateRandomTimeInterval() time.Duration {
+	// (inclusive)
+	min, max := 30, 120
+	randomSeconds := rand.Intn(max-min+1) + min
+
+	// Convert the random number of seconds to a time.Duration and return
+	return time.Duration(randomSeconds) * time.Second
+}
+
+// Flags for algorithm to reconfigure in shorter time periods for Project Demo
+
+// Factory Functions for endpoint generation
+
+// Memento Function // Prevent SQL Injection/Brute Force?
+
+// MTD Algorithm with a time interval, calls strategy to reconfiggure code

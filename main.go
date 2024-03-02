d1f343687b0ab56696b9c84642820d3698f10cd5
Chewbacca <thebopperm@gmail.com>
2024-03-01 22:08:50 -0600

    Added Static File Route Setup Go

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+// Endpoint routes to file names i.e. Key : Value
+func setupStaticFiles(app *gin.Engine) {
+	staticFiles := map[string]string{
+		"/public/script.js":   "./src/public/script.js",
+		"/assets/favicon.ico": "./src/assets/favicon.ico",
+		"/src/app.html":       "./app/my-skeleton-app/app.html",
+		"/":                   "./app/my-skeleton-app/login.html",
+	}
+
+	for urlPath, filePath := range staticFiles {
+		app.StaticFile(urlPath, filePath)
+	}
+}
+
 func main() {
 
 	app := gin.Default()
-
+	setupStaticFiles(app)
 	app.Run(":8080")
 
 }
